be53f431215009ab4e3ef4d5be5efc955c3da605
Christopher <ckaliszewski@gmail.com>
2025-04-11 13:02:25 -0400

    add logging

--- main.rs
+++ main.rs
@@ -1,4 +1,30 @@
+use std::io::Write;
+
+use chrono::Utc;
+use log::{LevelFilter, info, trace};
+
 /// Application entry point.
 fn main() {
-    println!("Hello, world!");
+    initialize_logger();
+
+    info!("Welcome to the Azure DevOps Gantt tool!");
+}
+
+/// Initialize logger to be used throughout the application.
+fn initialize_logger() {
+    env_logger::Builder::new()
+        .format(|buffer, record| {
+            writeln!(
+                buffer,
+                "{} [{:<5}] {}",
+                Utc::now().format("%Y-%m-%d %H:%M:%SZ"),
+                record.level(),
+                record.args(),
+            )
+        })
+        .filter_module(env!("CARGO_PKG_NAME"), LevelFilter::Trace)
+        .filter_level(LevelFilter::Off)
+        .init();
+
+    trace!("Application logger has been initialized!");
 }
