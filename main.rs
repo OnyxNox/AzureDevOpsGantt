319be5a3f791037e33a740223b5130675d4e1f2b
Christopher <ckaliszewski@gmail.com>
2025-04-11 13:25:51 -0400

    add input configuration file

--- main.rs
+++ main.rs
@@ -1,13 +1,24 @@
-use std::io::Write;
+mod models;
+
+use std::{
+    fs::File,
+    io::{BufReader, Write},
+};
 
 use chrono::Utc;
-use log::{LevelFilter, info, trace};
+use log::{LevelFilter, debug, info, trace};
+
+use crate::models::Configuration;
 
 /// Application entry point.
 fn main() {
     initialize_logger();
 
     info!("Welcome to the Azure DevOps Gantt tool!");
+
+    let configuration = read_configuration();
+
+    info!("Configuration Organization: {}", configuration.organization);
 }
 
 /// Initialize logger to be used throughout the application.
@@ -27,4 +38,25 @@ fn initialize_logger() {
         .init();
 
     trace!("Application logger has been initialized!");
+}
+
+/// Read in the configuration file.
+fn read_configuration() -> Configuration {
+    let configuration_file_path = "./.data/configuration.json";
+
+    debug!(
+        "Reading in the configuration file. File Path: {}",
+        configuration_file_path
+    );
+
+    let configuration_file =
+        File::open(configuration_file_path).expect("failed to open the configuration file");
+    let configuration_file_reader = BufReader::new(configuration_file);
+
+    let configuration = serde_json::from_reader(configuration_file_reader)
+        .expect("failed to parse the configuration file");
+
+    info!("Configuration file has been read in successfully!");
+
+    configuration
 }
