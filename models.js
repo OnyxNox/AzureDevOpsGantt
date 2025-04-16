270cb80b15f40df901fcbdfe6b95d3e049613393
Christopher <ckaliszewski@gmail.com>
2025-04-16 10:46:00 -0400

    stub out customization settings

--- models.js
+++ models.js
@@ -7,10 +7,12 @@ const Settings = (function () {
     return {
         dependencyRelation: "Tests",
         effortField: "Microsoft.VSTS.Scheduling.RemainingWork",
+        effortFieldTimeSpan: "d",
         featureStartDateField: "Microsoft.VSTS.Scheduling.StartDate",
         priorityField: "Microsoft.VSTS.Common.Priority",
         resourceCount: 1,
         sanitizationReplacement: '_',
+        sectionTagPrefix: "Section:",
         titleField: "System.Title",
         selectedDiagramType: DiagramType.Gantt,
     };
